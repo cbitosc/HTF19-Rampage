9f3fdb21b7d5ef1f196a5677ea6d1b0e93bfc89c
koushikpatnam <57003805+koushikpatnam@users.noreply.github.com>
2019-10-26 07:48:28 +0530

    Add files via upload

---
+++ questions.php
@@ -0,0 +1,16 @@
+<?php
+include("dbcon3.php");
+session_start();
+$re=$_SESSION['username'];
+$arr=array_keys($_POST);
+print_r($_POST);
+$rk=$arr[0];
+$q="INSERT INTO hel(username,hosid) VALUES ('$re','$rk') ";
+$res=mysqli_query($connection,$q);
+echo mysqli_error($connection);
+ header('Refresh:0; url="kalinga.php"');
+echo '<script language="javascript">';
+echo 'alert("emergency in the location")';
+echo '</script>';
+
+?>
