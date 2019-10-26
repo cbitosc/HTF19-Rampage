605ca53fd31602ee0a0d09459dc8fcc057eb784d
koushikpatnam <57003805+koushikpatnam@users.noreply.github.com>
2019-10-26 07:53:39 +0530

    Add files via upload

---
+++ details.php
@@ -0,0 +1,49 @@
+<table border="1">
+    <tr>
+        <th>name</th>
+        <th>location</th>
+        <th>exact address</th>
+        <th>phoneno</th>
+        <th>click here to send message</th>
+    </tr>
+
+<?php
+include('db2con.php');
+$ro=$_POST['pq'];
+if($ro)
+{
+$po="SELECT * FROM tab1 where location='$ro'";
+$re=mysqli_query($connection,$po);
+
+while($rows=mysqli_fetch_array($re)){
+    $a=$rows['id'];
+    echo "<tr>";
+    echo "<td>";
+    echo $rows['name'];
+    echo "</td>";
+    echo "<td>";
+    echo $rows['location'];
+    echo "</td>";
+    echo "<td>";
+    echo $rows['exact address'];
+    echo "</td>";
+     echo "<td>";
+    echo $rows['phone number'];
+    echo "</td>";
+     echo "<td>";
+    echo "
+    <form action='questions.php' method='POST'>
+    <input type='submit' name='$a' value='submit' >
+    </form>";
+    echo "</td>";
+    echo "<br/>";
+    
+}
+}
+else
+{
+    echo"not possible";
+}
+?>
+</table>
+
